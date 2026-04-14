d1e9d0628d4e4cb45215a4892a457a3a20454dc8
neiht12 <ducthien12112002@gmail.com>
2026-04-15 01:47:22 +0700

    Fixing calling null event because of commenting edit and delete

--- nutrition-food.js
+++ nutrition-food.js
@@ -273,9 +273,6 @@ function renderFoodDetail(category) {
                 </div>
             </div>
             <div class="food-detail-text">${escapeHtml(food.detail_desc || '')}</div>
-            <div style="margin-top: 12px; font-size: 0.85rem; color: #999;">
-                <span>⏱️ Sửa lần cuối: ${escapeHtml(food.modifiedAt)}</span>
-            </div>
         `;
 
         const imgEl = detailEl.querySelector('.food-detail-image');
